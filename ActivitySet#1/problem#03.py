bf1f80a59883063201f692b9f1581ce1664b5cfa
Omar <93114642+OmarYunus420@users.noreply.github.com>
2022-05-27 03:47:26 +0000

    P3 and p4

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -1,3 +1,5 @@
-# Variables, Expressions & Statements
-
-hrs = float(input("Enter hours? "))
+def mult(hrs,rate):
+    return hrs*rate
+hrs=float(input("Enter number of hours: "))
+rate=float(input("Enter the rate: "))
+print("Pay:", mult(hrs,rate))
