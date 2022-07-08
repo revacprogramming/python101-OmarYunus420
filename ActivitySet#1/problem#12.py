2b9d285f76dd01a1a7cf76d429d78280f728f275
Omar <93114642+OmarYunus420@users.noreply.github.com>
2022-07-08 03:33:57 +0000

    p12

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,1 +1,13 @@
+import re
 
+hand = open('regex_sum_1551682.txt')
+x=list()
+for line in hand:
+     y = re.findall('[0-9]+',line)
+     x = x+y
+
+sum=0
+for z in x:
+    sum = sum + int(z)
+
+print(sum)
